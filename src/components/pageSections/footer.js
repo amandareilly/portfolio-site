65413b3ab78110e66c8248753f060b9a6167a7a7
Amanda Reilly <amandamichellereilly@gmail.com>
2018-12-20 22:34:11 -0800

    Complete styling for homepage.

--- src/components/pageSections/footer.js
+++ src/components/pageSections/footer.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 import '../../css/pageSections/footer.scss';
 
 export default function Footer(props) {
     return (
         <footer className="footer-container">
-            This is the footer.
+            <div className="footer-link-wrapper">
+                <a href='https://github.com/amandareilly' aria-label='View my GitHub profile.' className='footer-link' target='_blank' rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+                <a href='https://linkedin.com/in/amandareilly-sd' aria-label="View my LinkedIn profile" className='footer-link' target='_blank' rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+                <a href='mailto:amanda@amandareilly.me' aria-label="Email me at amanda@amandareilly.me" className='footer-link' target='_blank' rel="noopener noreferrer"><i class="fas fa-envelope"></i></a>
+            </div>
         </footer>
     );
 }
--- src/components/views/home.js
+++ src/components/views/home.js
@@ -8,14 +8,20 @@ import '../../css/views/home.scss';
 export default function ComponentName(props) {
     return (
         <div className="home home-container">
-            <img className="headshot" src={headshot} alt="Amanda Reilly" />
-            <p class="greeting">Hi, I'm Amanda.</p>
-            <p class="intro-text">I'm a full-stack developer with a passion for problem-solving.</p>
-            <p class="intro-text">When I'm not writing code, you'll find me either traveling the world or reading a book.</p>
-            <p class="intro-text">Actually, it's a good bet that you'll find me doing both at once.</p>
+            <div className="row-wrapper">
+                <div className="headshot-wrapper">
+                    <img className="headshot" src={headshot} alt="Amanda Reilly" />
+                    <p className="greeting">Hi, I'm Amanda.</p>
+                </div>
+                <div className="intro-text-wrapper">
+                    <p className="intro-text">I'm a full-stack developer with a passion for problem-solving.</p>
+                    <p className="intro-text">When I'm not writing code, you'll find me either traveling the world or reading a book.</p>
+                    <p className="intro-text">Actually, it's a good bet that you'll find me doing both at once.</p>
+                </div>
+            </div>
             <div className="link-box">
-                <Link to='/about' className='read-more about'>Read More</Link>
-                <Link to='/projects' className='read-more projects'>View Projects</Link>
+                <Link to='/about' className='read-more about'>About Me</Link>
+                <Link to='/projects' className='read-more projects'>My Projects</Link>
             </div>
         </div>
     );
